662a066784b7480b4a24bf86f964115593a512ee
Lucia Gordon <luciagordon@g.harvard.edu>
2024-03-31 16:46:11 -0400

    Added demo and made batch updating happen continuously

--- static/script.js
+++ static/script.js
@@ -1,12 +1,15 @@
+// global variables
 let batch = 1;
-let batch_identifiers = [];
-let current_index = 0;
-let current_modality = 'thermal';
-let current_zoom = 'in';
-let batch_labels = [];
-let intervalID = setInterval(() => checkImageExists(), 5000); // poll the server every 5 seconds
+let batchIdentifiers = [];
+let currentIndex = 0;
+let currentModality = 'thermal';
+let currentZoom = 'in';
+let batchLabels = [];
+let batchInterval = 0;
+let demo = false;
+let imageFolderName = 'images';
 const batchSize = 10;
-const spans = document.getElementsByTagName('span');
+const spans = document.getElementsByClassName('modality-span');
 
 function adjustButtonSize() {
     const imgDim = document.getElementsByTagName('img')[0].offsetWidth;
@@ -46,11 +49,81 @@ function adjustFontSize() {
     }
 }
 
+function setBatchMessage() {
+    document.getElementById('starting-options').style.display = 'none';
+    document.getElementById('batch-message').innerText = `Waiting for batch ${batch}...`;
+    document.getElementById('batch-message').style.visibility = 'visible';
+}
+
+function getStartingBatchNumber() {
+    fetch('/get-starting-batch-number')
+    .then(response => response.json())
+    .then(data => batch = data.batch_number)
+    .then(setBatchMessage());
+}
+
+function updateBatch() {
+    console.log(`batch number in update batch: ${batch}`);
+    console.log(`batch identifiers in update batch: ${batchIdentifiers}`);
+    fetch('/update-batch', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify([imageFolderName, batch, batchIdentifiers])})
+        .then(response => response.json())
+        .then(data => batchIdentifiers = data.batch_identifiers);
+}
+
+function checkImageExists() {
+    let nextImageExists = batchIdentifiers.length > currentIndex;
+
+    if (nextImageExists) {
+        showImage();
+    } else {
+        setTimeout(checkImageExists, 100);
+    }
+}
+
+function startLabeling() {
+    fetch('/run-cnn');
+    getStartingBatchNumber();
+    batchInterval = setInterval(() => updateBatch(), 100);
+    checkImageExists();
+}
+
 function initiate() {
+    adjustButtonSize();
+    adjustFontSize();
+
     window.addEventListener('resize', function() {
         adjustButtonSize();
         adjustFontSize();
-    });    
+    });
+
+    document.getElementById('code').addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+          if (this.value == 'nnjOBaem54') { // if the user enters the right code
+              startLabeling();
+          } else {
+              document.getElementById('result').innerText = 'The code you entered is incorrect.';
+          }
+        }
+      });
+    
+    window.addEventListener('beforeunload', function (e) { // upon window close
+        if (!demo) {
+            fetch('/stop-cnn');
+        }
+    });
+}
+
+function activateDemo() {
+    demo = true;
+    imageFolderName = 'demo-images';
+    setBatchMessage();
+    setTimeout(function() {
+        updateBatch();
+    }, 3000);
+    checkImageExists();
 }
 
 function setButtonVisibility(value) {
@@ -61,89 +134,87 @@ function setButtonVisibility(value) {
     }
 }
 
-function checkImageExists() {
-    fetch('/check_image_exists', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify([batch, batch_identifiers])})
-        .then(response => response.json())
-        .then(data => {
-            if (data.image_exists) {
-                batch_identifiers.push(data.next_identifier);
-                document.getElementById('batch message').innerText = `Batch ${batch}: Image ${current_index+1}/${batchSize}, ID = ${batch_identifiers[current_index]}`;
-                document.getElementsByTagName('img')[0].style.visibility = 'visible';
-                showImage();
-                adjustButtonSize();
-                adjustFontSize();
-                setButtonVisibility('visible');
-                clearInterval(intervalID); // stop checking for the path
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
-
 function getOtherModality(){
-    if (current_modality == 'thermal') {
+    if (currentModality == 'thermal') {
         return 'rgb';
-    } 
+    }
+
     return 'thermal';
 }
 
 function getOtherZoom(){
-    if (current_zoom == 'in') {
+    if (currentZoom == 'in') {
         return 'out';
-    } 
+    }
+
     return 'in';
 }
 
 function setModality(modality) {
-    current_modality = modality;
+    currentModality = modality;
 }
 
 function setZoom(zoom) {
-    current_zoom = zoom;
+    currentZoom = zoom;
 }
 
 function showImage() {
-    document.getElementsByTagName('img')[0].src = `static/images/batch-${batch}-images/${batch_identifiers[current_index]}/${current_modality}-${current_zoom}.png`;
-    document.getElementById(current_modality).style.backgroundColor = 'green';
+    document.getElementById('batch-message').innerText = `Batch ${batch}: Image ${currentIndex+1}/${batchSize}, ID = ${batchIdentifiers[currentIndex]}`;
+    document.getElementsByTagName('img')[0].src = `static/${imageFolderName}/batch-${batch}-images/${batchIdentifiers[currentIndex]}/${currentModality}-${currentZoom}.png`;
+    document.getElementsByTagName('img')[0].style.visibility = 'visible';
+    document.getElementById(currentModality).style.backgroundColor = 'green';
     document.getElementById(getOtherModality()).style.backgroundColor = 'gray';
-    document.getElementById(current_zoom).style.backgroundColor = 'green';
+    document.getElementById(currentZoom).style.backgroundColor = 'green';
     document.getElementById(getOtherZoom()).style.backgroundColor = 'gray';
+    setButtonVisibility('visible');
+}
+
+function reset() {
+    batch += 1;
+    batchIdentifiers = [];
+    currentIndex = 0;
+    batchLabels = [];
+    document.getElementById('batch-message').innerText = `Waiting for batch ${batch}...`;
+    setButtonVisibility('hidden');
+    document.getElementsByTagName('img')[0].style.visibility = 'hidden';
 }
 
 function next() {
-    current_index += 1;
-    
-    if (current_index < batchSize) { // next image
-        current_modality = 'thermal';
-        current_zoom = 'in';
-        intervalID = setInterval(() => checkImageExists(), 1000); // poll the server every 1 second
-    } else { // next batch
-        fetch('/save_batch_labels', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify([batch, batch_identifiers, batch_labels])});
+    currentIndex += 1;
 
-        batch += 1;
-        batch_identifiers = [];
-        current_index = 0;
-        batch_labels = [];
-        document.getElementById('batch message').innerText = `Waiting for batch ${batch}...`
-        setButtonVisibility('hidden');
-        document.getElementsByTagName('img')[0].style.visibility = 'hidden';
-        intervalID = setInterval(() => checkImageExists(), 5000); // poll the server every 5 seconds
+    if (currentIndex < batchSize) { // next image
+        currentModality = 'thermal';
+        currentZoom = 'in';
+    } else { // next batch
+        if (demo) {
+            reset();
+            setTimeout(function() {
+                updateBatch();
+            }, 3000);
+        } else {
+            clearInterval(batchInterval);
+
+            fetch('/save-batch-labels', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify([batch, batchIdentifiers, batchLabels])})
+            .then(response => response.text())
+            .then(() => {
+                reset();
+                batchInterval = setInterval(() => updateBatch(), 100);
+            });
+        }
     }
+
+    checkImageExists();
 }
 
 function middenLabel() {
-    batch_labels.push(1);
+    batchLabels.push(1);
     next();
 }
 
 function emptyLabel() {
-    batch_labels.push(0);
+    batchLabels.push(0);
     next();
 }
